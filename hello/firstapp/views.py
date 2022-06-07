4518e2b297c1d37fb6fc6a4513dfb72833bf5db0
Andrey <AndreyHlopkov@gmaile.com>
2022-06-07 22:30:27 +0300

    change def index(), add more data to context

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -4,9 +4,12 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanen
 
 def index(request):
     # return render(request, 'firstapp/home.html')
-    data = {'header': 'Передача параметров в шаблон Django',
-            'message': 'Загружен шаблон templates/firstapp/index_app1.html'}
-    return render(request, 'firstapp/index_app1.html', context=data)
+    header = 'Персональные данные'
+    langs = ['Английский', 'Немецкий', 'Испанский']
+    user = {'name': 'Максим', 'age': 30}
+    addr = ('Виноградная', 23, 45)
+    data = {'header': header, 'langs': langs, 'user': user, 'address': addr}
+    return render(request, 'index.html', context=data)
 
 def about(request):
     return HttpResponse('About')
